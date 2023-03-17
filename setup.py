435f471dbdd10473cbda1cee58ac1a3e25fbca47
Geetesh Lokhande <lokhandegeetesh@gmail.com>
2023-03-17 11:32:59 -0500

    setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT ='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''This Function will return the list of requirements'''
+    requirements =[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+
+# consider as meta data for the project
+setup(
+    name= 'mlproject',
+    version='0.0.1',
+    author='Geetesh',
+    author_email='lokhandegeetesh@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
